e6f3906faa834290065bfac5d345134d4f879d5f
laucavv <laucavv23@gmail.com>
2020-08-15 00:58:28 +0000

    fix puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,23 +1,35 @@
-#  Add a custom HTTP header with Puppet
-exec { 'update':
-    command => 'sudo apt-get update',
-    path    => ['/usr/bin', '/bin'],
+# Add a custom HTTP header with Puppet
+exec {'update':
+  command => '/usr/bin/ sudo apt-get -y update',
 }
 
-package { 'nginx web server':
+package {'nginx':
   ensure => installed,
   name   => 'nginx'
 }
 
-file_line { 'custom_header':
+file { '/var/www/html/index.html':
   ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => "add_header X-Served-By ${hostname};",
-  require => Package['nginx'],
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School'
 }
-service { 'nginx':
-    ensure     => running,
-    require    => Package['nginx'],
-    hasrestart => true
+
+file_line { 'redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=wDjeBNv6ip0 permanent;'
+}
+
+file_line {'header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $HOSTNAME;'
+}
+
+service { 'service nginx':
+  ensure     => running,
+  require    => Package['nginx'],
+  hasrestart => true
 }
