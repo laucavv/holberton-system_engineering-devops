7f6a6c2d624a9ac3bbc66f399de1cc036da1d0e5
laucavv <laucavv23@gmail.com>
2020-09-17 20:07:23 -0500

    subreddits

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Write a function that queries the Reddit API and
+    returns the number of subscriber
+"""
+import requests
+from sys import argv
+def number_of_subscribers(subreddit):
+    """ The fuction returns the number of subscribers """
+    url_r = "https://api.reddit.com/r/{}/about".format(subreddit)
+    request_t = requests.get(url_r, headers={'User-Agent': 'Python3'})
+    if str(request_t) == '<Response [200]>':
+        return request_t.json().get('data').get('subscribers')
+    return 0
